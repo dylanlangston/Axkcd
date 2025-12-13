70090463d1acdff93780d8ac67de2ef32a0ad0a6
Dylan <dylanlangston@gmail.com>
2025-12-13 07:42:32 +0000

    Improve browser initialization

--- src/AvaloniaXKCD.Tests/TestBases/BrowserBaseTest.cs
+++ src/AvaloniaXKCD.Tests/TestBases/BrowserBaseTest.cs
@@ -1,8 +1,26 @@
 namespace AvaloniaXKCD.Tests.TestBases;
 
-public abstract class BrowserBaseTest(string browser) : PageTest
+public abstract class BrowserBaseTest : PageTest
 {
-    public override string BrowserName => browser;
+    private readonly string _browserName;
+
+    protected BrowserBaseTest(string browser)
+        : base(new BrowserTypeLaunchOptions
+        {
+            Timeout = 90_000, // 90 second timeout for browser launch
+            Args =
+            [
+                // Flags for limited resource environments like CI
+                "--disable-dev-shm-usage",
+                "--no-sandbox",
+                "--disable-setuid-sandbox"
+            ]
+        })
+    {
+        _browserName = browser;
+    }
+
+    public override string BrowserName => _browserName;
 
     [ClassDataSource<AvaloniaBrowserProject>(Shared = SharedType.PerAssembly)]
     public required AvaloniaBrowserProject AvaloniaManager { get; init; }
